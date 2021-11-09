d384d0662dc864a713d661723d945e5260ebc412
Miguel <miguelandradebarreto2@gmail.com>
2021-11-09 14:05:17 -0300

    :sparkles: add connection with db

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const db = require("./db");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hellow wworld"));
+app.get("/users/:id", (req, res) => {
+  const getUserQuery = `SELECT * FROM client WHERE i_client_client = ${req.params.id}`;
+
+  db().query(getUserQuery, (error, results) => {
+    if (error) res.json(error);
+    res.json(results);
+  });
+});
 
 app.listen(3000, () => {
   console.log("Running at 3000");
--- db.js
+++ db.js
@@ -1,28 +1,20 @@
-const mysql = require("mysql");
+require("dotenv").config();
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  user: "root",
-  password: "Miguel@1234",
-  database: "cfbcursos",
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DB,
+  password: process.env.PASSWORD,
 });
 
-connection.connect(function (err) {
-  if (err) return console.log(err);
-  console.log("conectou!");
-  createTable(connection);
-});
-
-function createTable(conn) {
-  const sql =
-    "CREATE TABLE IF NOT EXISTS Clientes (n" +
-    "ID int NOT NULL AUTO_INCREMENT,n" +
-    "Nome varchar(150) NOT NULL,n" +
-    "CPF char(11) NOT NULL,n" +
-    "PRIMARY KEY (ID)n" +
-    ");";
-
-  conn.query(sql, function (error, results, fields) {
-    if (error) return console.log(error);
-    console.log("criou a tabela!");
+const db = () => {
+  connection.connect(function (err) {
+    if (err) console.log(`DB CDonnection failed ${err}`);
+    else console.log(`DB connection successfully`);
   });
-}
+
+  return connection;
+};
+
+module.exports = db;
