f9ec42bb0c6d515b05e68b57f94d2652d47fe6db
Miguel <miguelandradebarreto2@gmail.com>
2021-11-10 19:23:48 -0300

    :sparkles: add .dockerignore

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require("express");
-// const db = require("./db");
+const db = require("./db");
 
 const app = express();
 
-// app.get("/users/:id", (req, res) => {
-//   const getUserQuery = `SELECT * FROM client WHERE i_client_client = ${req.params.id}`;
+app.get("/users/:id", (req, res) => {
+  const getUserQuery = `SELECT * FROM client WHERE i_client_client = ${req.params.id}`;
 
-//   db().query(getUserQuery, (error, results) => {
-//     if (error) res.json(error);
-//     res.json(results);
-//   });
-// });
+  db().query(getUserQuery, (error, results) => {
+    if (error) res.json(error);
+    res.json(results);
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("tst");
--- db.js
+++ db.js
@@ -1,20 +1,20 @@
-// require("dotenv").config();
-// const mysql = require("mysql2");
+require("dotenv").config();
+const mysql = require("mysql2");
 
-// const connection = mysql.createConnection({
-//   host: process.env.HOST,
-//   user: process.env.USER,
-//   database: process.env.DB,
-//   password: process.env.PASSWORD,
-// });
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DB,
+  password: process.env.PASSWORD,
+});
 
-// const db = () => {
-//   connection.connect(function (err) {
-//     if (err) console.log(`DB CDonnection failed ${err}`);
-//     else console.log(`DB connection successfully`);
-//   });
+const db = () => {
+  connection.connect(function (err) {
+    if (err) console.log(`DB CDonnection failed ${err}`);
+    else console.log(`DB connection successfully`);
+  });
 
-//   return connection;
-// };
+  return connection;
+};
 
-// module.exports = db;
+module.exports = db;
